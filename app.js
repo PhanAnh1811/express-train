4ea51f311c36a012d4e3023cdec5e515513fdd1f
PhanAnh1811 <tuananh.61779@gmail.com>
2021-04-20 19:42:25 +0700

    update

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ require("@babel/core").transform("code", {
 
 
 import express from 'express'
+import shopRoute from './src/routers/shopRoute'
+
+const app = express()
+
+app.listen(8080,() => {
+  console.log("Connected server successfully")
+})
+
+app.use('/shops',shopRoute)
